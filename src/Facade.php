71d32b59b4fc50bf1d92313d2604955d806c945a
pvankouteren <pvankouteren@gmail.com>
2022-08-10 10:29:03 +0200

    Updated for higher php and guzzle versions. Small code cleanup and re-indenting

--- src/Facade.php
+++ src/Facade.php
@@ -6,13 +6,13 @@ use Illuminate\Support\Facades\Facade as IlluminateFacade;
 
 class Facade extends IlluminateFacade
 {
-	/**
-	 * Get the registered name of the component.
-	 *
-	 * @return string
-	 */
-	protected static function getFacadeAccessor()
-	{
-		return 'pico';
-	}
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'pico';
+    }
 }
